671b094a1ffbf2cdecb6eae9bcd66f45685ab924
Administrator <admin@example.com>
2017-09-09 02:05:18 -0400

    oldsited2

--- resources/views/includes/menu.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="btn-group">
-<button class="btn dropdown-toggle waves-effect waves-light" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-plus" aria-hidden="true"></i></button>
-
-<div class="dropdown-menu">
-<ul>
-<li><a href="#" id="btn-add">Entries</a></li>
-</ul>
-</div>
-</div>
--- setup.sh
+++
@@ -1,4 +0,0 @@
-composer install
-chmod -R gu+w storage && chmod -R guo+w storage && chmod 777 -R * && chown apache:apache -R * && php artisan cache:clear && php artisan view:clear
-cp -a httpd.conf /etc/httpd/conf/httpd.conf
-echo PASS
